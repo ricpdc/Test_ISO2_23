2e40343d32846b06c315e61d95bfb7efa7e6a63d
Ricardo <ricardo.pdelcastillo@uclm.es>
2023-10-03 19:22:22 +0200

    initial commit

---
+++ library/src/main/java/es/uclm/library/business/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package es.uclm.library.business.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import es.uclm.library.business.entity.Greeting;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greetingForm(Model model) {
+		model.addAttribute("greeting", new Greeting());
+		return "greeting";
+	}
+
+	@PostMapping("/greeting")
+	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model) {
+		model.addAttribute("greeting", greeting);
+		return "result";
+	}
+}
